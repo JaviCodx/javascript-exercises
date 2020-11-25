44b5121b0e33b76798a0b2d78ce9985dca397148
Javier Saiz <javiersaizcano@gmail.com>
2020-11-25 13:27:27 +0100

    Completed repeatString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,4 +1,19 @@
-const repeatString = function() {
+const repeatString = function(string, n) {
+
+if (n<0) {
+    return 'ERROR';
+}
+
+else {
+    let newString = '';
+
+    for (let i=0; i<n; i++){
+    newString = newString + string; ;
+    }
+    
+    return newString;
+}
+
 
 }
 
