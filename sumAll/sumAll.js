1fd0895aa4457e092171ae3cdf8eb4c6183a4d47
Javier Saiz <javiersaizcano@gmail.com>
2020-11-25 15:41:37 +0100

    Completed sumAll.js

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,4 +1,23 @@
-const sumAll = function() {
+const sumAll = function(n1, n2) {
+let sum = 0;
+
+
+if (n1<0 || n2 < 0 || !Number.isInteger(n1) || !Number.isInteger(n2)){
+    return 'ERROR'
+}
+if (n1>n2){
+    let temp = n1;
+    n1 = n2;
+    n2 = temp;
+}
+
+
+for (let i=n1 ; i<=n2; i++){
+
+    sum +=i;
+}
+
+return sum;
 
 }
 
