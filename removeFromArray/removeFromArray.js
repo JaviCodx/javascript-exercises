10ba665f630ff182d04d46970ca77725542565da
Javier Saiz <javiersaizcano@gmail.com>
2020-11-25 15:26:26 +0100

    Completed removeFromArray

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
 
+let myArray =args[0];
+
+let newArray = myArray.filter(function(value, index, arr){ 
+    return !args.includes(value);
+});
+
+return newArray;
 }
 
 module.exports = removeFromArray
--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,4 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) {
+
+
+let temp = '';
+
+
+for (let i = string.length - 1 ; i>=0; i--){
+
+    temp += string[i];
+}
+
+return temp;
 
 }
 
